6c943b9c892f6d875a5f733d386fb6ff8e190d8e
Leon Zhang <lzha@microsoft.com>
2021-09-01 22:43:15 +0800

    M1

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # os-perf
+
+
+# Prerequisite
+# Redhat&CentOS
+# setup to collect process information
+# install sysstat
+yum install -y sysstat
+# start sysstat daemon
+systemstl enable sysstat --now
+
+
+# clone the repo
+
+git clone https://github.com/ktzyl/os-perf.git
+cd os-perf
+chmod +x pidstat.sh
+. 
--- pidstat.sh
+++ pidstat.sh
@@ -1,21 +1,27 @@
-# create tmp dir
+#! /bin/bash
 dir="/tmp/pidperf"
 if [ -d $dir ]
 then
-    echo "Directory already exists. Exiting..."
-    exit 1
+    read -p "$dir already exists. Do you want to remove it? [y/n]" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+      exit 1
+    else
+      rm -rf $dir
+      mkdir $dir
+    fi
 else
-    mkdir $dir
+  mkdir $dir
 fi
 cd $dir
-
 while true;
-do 
+do
     pidstat -u 15 > pidcpu_$(date "+%Y-%m-%d-%H-%M").log & disown $!
     export PIDCPUJOB=$(echo $!)
     pidstat -r 15 > pidmem_$(date "+%Y-%m-%d-%H-%M").log & disown $!
-    export PIDMEMJOB=$!
+    export PIDMEMJOB=$(echo $!)
     sleep 180
     kill $PIDCPUJOB
     kill $PIDMEMJOB
-done $
+done
